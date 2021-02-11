121dcea0212f920708ff6eb46ef54a652cf18c0f
Silver Abel <silverabel66@gmail.com>
2021-02-11 16:24:36 +0200

    post, delete, patch vabandus

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-const vabandused = [
+app.use(express.json());
+
+let vabandused = [
   {
     id: 1,
     description: 'Ei tahtnud teha'
@@ -25,6 +27,26 @@ app.get('/vabandused', (req, res) => {
 app.get('/vabandused/:id', (req, res) => {
   let vabandus = vabandused.find(vabandus => vabandus.id == req.params.id);
   vabandus ? res.status(200).json({vabandus: vabandus.description}) : res.status(404).json({message: 'Not found'});
+});
+
+app.post('/vabandused', (req, res) => {
+  const description = req.body.description;
+  vabandused.push({id: vabandused.length + 1, description});
+  res.status(201).json({message: 'success'});
+});
+
+app.delete('/vabandused/:id', (req, res) => {
+  vabandused = vabandused.filter(vabandus => vabandus.id != req.params.id);
+  res.status(200).json({message: 'Kustutatud'});
+});
+
+app.patch('/vabandused/:id', (req, res) => {
+  const description = req.body.description;
+  vabandused = vabandused.map(function(vabandus) {
+    if (vabandus.id == this.id) vabandus.description = description;
+    return vabandus;
+  }, req.params);
+  res.status(200).json({message: 'Success'});
 });
 
 app.listen(3000, () => console.log('Server go brr'));
